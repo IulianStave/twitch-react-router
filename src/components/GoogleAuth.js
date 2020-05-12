b890698cf0497bb3636e000b7e38d3a96dfdb429
iulianstave <iulian.staverescu@gmail.com>
2020-05-12 17:39:56 +0300

    Update helper methods names

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -25,30 +25,30 @@ class GoogleAuth extends React.Component {
   }
 
   onAuthChange = () => {
-    this.setState({isSignedIn: this.auth.isSignedIn.get()});
-  }
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
 
-  onSignIn = () => {
+  onSignInClick = () => {
     this.auth.signIn();
-  }
+  };
 
-  onSignOut = () => {
+  onSignOutClick = () => {
     this.auth.signOut();
-  }
+  };
 
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-        <button onClick={this.onSignOut} className="ui red google button">
+        <button onClick={this.onSignOutClick} className="ui red google button">
           <i className="google icon" />
           Sign Out
         </button>
       );
     } else {
       return (
-        <button onClick={this.onSignIn} className="ui blue google button">
+        <button onClick={this.onSignInClick} className="ui blue google button">
           <i className="google icon" />
           Sign In with Google
         </button>
