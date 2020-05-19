97e73659d98ed153b6ed899076d1a1f83e1f9baa
iulianstave <iulian.staverescu@gmail.com>
2020-05-19 16:11:24 +0300

    Update input

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,22 +1,27 @@
 import React from "react";
-import { Field, reduxForm } from 'redux-form';
+import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput(formProps){
-    console.log(formProps);
-    return <input/>;
+  renderInput(formProps) {
+    // console.log(formProps);
+    return (
+      // <input
+      //   onChange={formProps.input.onChange}
+      //   value={formProps.input.value}
+      // />
+       <input {...formProps.input} />
+    );
   }
   render() {
     return (
       <form>
         <Field name="title" component={this.renderInput} />
         <Field name="description" component={this.renderInput} />
-
       </form>
-      );
+    );
   }
 }
 
 export default reduxForm({
-  form: 'streamCreate'
+  form: "streamCreate",
 })(StreamCreate);
